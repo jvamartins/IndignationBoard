9019e3b1dcade2e6294a49a0cf6be75d340272ea
jvamartins <jvam90@outlook.com>
2021-06-17 11:09:06 -0300

    Mais fotos

--- src/Board.js
+++ src/Board.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import Hand from "./Hand";
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tses: this.getTSES(),
+      cses: this.getCSES(),
+      leadership: this.getLeadership(),
+    };
+  }
+
+  getTSES() {}
+
+  getCSES() {}
+
+  getLeadership() {}
+
   render() {
     return (
-      <div>
+      <div className="Board">
         <h1>Indignation Board, MADAFACKAS</h1>
-        <Hand />
+        <Hand title="TSEs" />
+        <Hand title="CSEs" />
+        <Hand title="LEADERSHIP" />
       </div>
     );
   }
--- src/Hand.js
+++ src/Hand.js
@@ -5,6 +5,7 @@ class Hand extends Component {
   render() {
     return (
       <div className="Hand card-group">
+        <h2>{this.props.title}</h2>
         <Card />
         <Card />
         <Card />
